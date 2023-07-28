e0439c642e1e31d1cf4da59739c1745a2d331ab6
Develon2015 <302615249@qq.com>
2023-07-28 23:56:13 +0800

    Bug fix: dump不完整

--- core/src/a2b.rs
+++ core/src/a2b.rs
@@ -2,27 +2,27 @@ use std::io;
 
 use tokio::net::tcp::{ReadHalf, WriteHalf};
 
-
 #[cfg(feature = "dump")]
 async fn copy(r: &mut tokio::net::tcp::ReadHalf<'_>, w: &mut tokio::net::tcp::WriteHalf<'_>) -> io::Result<u64> {
-    use tokio::{io::{AsyncReadExt, AsyncWriteExt}, fs};
+    use std::io::Write;
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
     use uuid::Uuid;
     use crate::time::get_time;
 
     let time = get_time();
     let path = format!("{time} {}.txt", Uuid::new_v4().to_string());
-    let mut file = fs::OpenOptions::new()
+    let mut file = std::fs::OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
         .open(&path.replace(":", "-"))
-        .await?;
+        .unwrap();
     let mut count = 0u64;
     let mut buf = [0; 8 * 1024]; // 8 KB
     loop {
         match r.read(&mut buf).await {
             Ok(n) if n > 0 => {
-                let _ = file.write_all(&buf[..n]).await;
+                let _ = file.write_all(&buf[..n]);
                 match w.write_all(&buf[..n]).await {
                     Err(_) => {
                         break Ok(count)
